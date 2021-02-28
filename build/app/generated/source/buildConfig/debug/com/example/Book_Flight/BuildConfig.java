ff444845070a015c1ae2cfde322ca2a8f51807e9
Dagmawi Yohannes <49404685+DeltaYankeeTango@users.noreply.github.com>
2021-02-28 22:00:12 +0300

    Book_Flight

---
+++ build/app/generated/source/buildConfig/debug/com/example/Book_Flight/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.Book_Flight;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.Book_Flight";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
